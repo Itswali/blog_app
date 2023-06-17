c90fbb3f5c220a4e3d90e5d632d3d7b9a0d35aee
Itswali <luckywali0986@gmail.com>
2023-06-18 01:51:03 +0500

    removed the migration files which was causing errors

--- db/migrate/20230616212113_remove_duplicate_user_id_from_likes.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDuplicateUserIdFromLikes < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :likes, :user_id
-  end
-end
--- db/migrate/20230617192156_add_user_id_to_likes.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToLikes < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :likes, :user, foreign_key: true
-  end
-end
