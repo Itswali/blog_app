b9a0bd9a1e2dfec368303f2e3d2607480edd87be
Itswali <luckywali0986@gmail.com>
2023-06-20 19:38:17 +0500

    add rails best practices on routes.rb file using resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
-  # root "articles#index"
   root 'users#index'
-  get '/users', to: 'users#index', as: :users
-  get '/users/:id', to: 'users#show', as: :user
-  get '/users/:user_id/posts', to: 'posts#index', as: :user_posts
-  get '/users/:user_id/posts/:id', to: 'posts#show', as: :user_post
-  get '/posts/new', to: 'posts#new', as: :new_post
-  post '/posts/new', to: 'posts#create'
+
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
+
+  resources :posts, only: [:new, :create]
+
   post 'comments/new', to: 'comments#create', as: 'create_comment'
   post 'likes/new', to: 'likes#create', as: 'create_like'
 end
