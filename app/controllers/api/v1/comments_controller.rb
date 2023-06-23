28fdfc4c8a7f49bc0e1b68dceb217d82959ca84a
NicolasAndrehh <nicolasolaya22@gmail.com>
2023-06-23 14:52:53 -0500

    Fix rubocop errors

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -33,7 +33,7 @@ class Api::V1::CommentsController < ApplicationController
   def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
-    
+
     redirect_to request.referrer
   end
 
